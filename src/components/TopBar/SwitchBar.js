5ea0b16c06f8339e1cc8878eafec8a4637c75af7
Hung Dung <truonghungdung007@gmail.com>
2023-03-31 14:03:51 +0700

    cp switch bar

--- src/components/TopBar/SwitchBar.js
+++ src/components/TopBar/SwitchBar.js
@@ -1,16 +1,60 @@
 import { Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SwitchBarStyle'
 import Button from '../Button/Button'
+import { TabView } from 'react-native-tab-view'
 
 const SwitchBar = (props) => {
-    const {mode = 'about',
-    } = props
+  const { mode = 'about',
+  } = props
+
+  let [TabViewIndex, setTabViewIndex] = useState(0);
+
+  const [routes] = useState([
+    {
+      key: 'fist',
+      title: 'About'
+    },
+    {
+      key: 'second',
+      title: 'Sessions'
+    },
+    {
+      key: 'second',
+      title: 'Sessions'
+    }
+  ])
+  const ButtonActive = {
+    btnStyle: [styles.ButttonDefault, styles.ButtonOpacityLight],
+    fontStyle: [styles.fontColorLight, styles.fontBtn],
+  }
+  const ButtonUnActive = {
+    btnStyle: [styles.ButttonDefault, styles.ButtonOpacityDark],
+    fontStyle: [styles.fontColorDark, styles.fontBtn],
+  }
+  const renderSwicthButtons = () => {
+    const html = routes.map((e, i) => {
+      return <Button
+        TypeTagChild={'Text'}
+        style={TabViewIndex === i ? ButtonActive.btnStyle : ButtonUnActive.btnStyle}
+        content={e.title}
+        ContentStyle={TabViewIndex === i ? ButtonActive.fontStyle : ButtonUnActive.fontStyle}
+        onPress={() => TabViewIndex !== i ? setTabViewIndex(i) : setTabViewIndex(TabViewIndex)} />
+    })
+    return html
+  }
   return (
     <View style={styles.container}>
-        <Button 
-         TypeTagChild={'icon'}
-         PatchImage={require('../../assets/img/icons/BackArrow.png')}/>
+      <View style={styles.container_title}>
+        <Button
+          TypeTagChild={'icon'}
+          PatchImage={require('../../assets/img/icons/BackArrow.png')}
+          style={styles.Button_ic} />
+        <Text style={styles.Title}>The Batman</Text>
+      </View>
+      <View style={styles.container_Btn}>
+        {renderSwicthButtons()}
+      </View>
     </View>
   )
 }
--- src/components/TopBar/SwitchBarStyle.js
+++ src/components/TopBar/SwitchBarStyle.js
@@ -3,9 +3,57 @@ import { StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
     container:{
-        paddingVertical:67,
+        width:'100%',
+        height:160,
         paddingHorizontal:32,
-        backgroundColor:'rgba(31, 41, 61, 1)'
+        backgroundColor:'rgba(31, 41, 61, 1)',
+        justifyContent:'center'
+    },
+    container_title:{
+        width:'100%',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    Button_ic:{
+        position:'absolute',
+        left:'2%',
+    },
+    Title:{
+        color:'white',
+        fontWeight:700,
+        fontSize:20,
+    },
+    container_Btn:{
+        position:'absolute',
+        bottom:0,
+        left:0,
+        right:0,
+        height:'36%',
+        flexDirection:'row'
+    },
+    ButttonDefault:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    ButtonOpacityLight:{
+        borderBottomWidth:4,
+        borderBottomColor:'#FF8036',
+    },
+    ButtonOpacityDark:{
+        borderBottomWidth:4,
+        borderBottomColor:'#637394',
+    },
+    fontBtn:{
+        fontSize:18,
+        fontWeight:'700',
+    },
+    fontColorLight:{
+        color:'#FF8036',
+    },
+    fontColorDark:{
+        color:'#637394',
     }
 })
 export default styles;
