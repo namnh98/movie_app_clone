e42ca9aff1109f68418a049e0e29da60b25030ad
Hung Dung <truonghungdung007@gmail.com>
2023-04-12 16:06:42 +0700

    cp session

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,35 +1,41 @@
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import React from "react";
 
 const Button = (props) => {
-    const {
-        TypeTagChild = '',
-        content = null,
-        ContentStyle,
-        PatchImage,
-        ImageStyle,
-        ...reset
-    } = props
+  const {
+    TypeTagChild = "",
+    content = null,
+    ContentStyle,
+    PatchImage,
+    ImageStyle,
+    ContainerStyle,
+    Selected = false,
+    SelectedStyle,
+    ...reset
+  } = props;
 
-    const Renderchild = () => {
-        switch (TypeTagChild) {
-            case 'Text':
-                return <Text style={ContentStyle}>{content}</Text>
-            case 'icon':
-                return <Image source={PatchImage} style={ImageStyle} />
-            default:
-                return <View />
-        }
+  const Renderchild = () => {
+    switch (TypeTagChild) {
+      case "Text":
+        return <Text style={ContentStyle}>{content}</Text>;
+      case "icon":
+        return <Image source={PatchImage} style={ImageStyle} />;
+      default:
+        return <View />;
     }
-    return (
-        <TouchableOpacity hitSlop={2} {...reset}>
-            {Renderchild()}
-        </TouchableOpacity>
-    )
-}
+  };
+  return (
+    <TouchableOpacity
+      hitSlop={2}
+      style={[ContainerStyle,Selected ? {backgroundColor:'rgba(255, 128, 54, 1)'} : {}]}
+      {...reset}
+    >
+      {Renderchild()}
+    </TouchableOpacity>
+  );
+};
 
-export default Button
+export default Button;
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
+// Selected ? SelectedStyle : {}
--- src/screens/SelectSeat/SelectSeat.js
+++ src/screens/SelectSeat/SelectSeat.js
@@ -7,14 +7,19 @@ import StausIcon from "../../components/TagIcon/StausIcon";
 import Button from "../../components/Button/Button";
 import { useNavigation } from "@react-navigation/native";
 import { SCREEN_NAME } from "../../constants/screenNames";
+import { check } from "prettier";
 
 const SelectSeat = () => {
-  const navigation=useNavigation()
-  const [isZoom,setisZoom] = React.useState(false)
-    let totalSeats = [];
-    for (let i = 0; i < 154; i++) {
-      totalSeats.push(i);
-    }
+  const navigation = useNavigation();
+  const [isZoom, setisZoom] = React.useState(false);
+  const [SelectedSeats,setSelectedSeats] = React.useState([]);
+  let totalSeats = [];
+  for (let i = 0; i < 154; i++) {
+    totalSeats.push({
+      id:`seat${i+1}`,
+      lable:`${i+1}`
+    });
+  }
   const renderIcon = () => {
     let rs = totalSeats.map((e, i) => {
       return (
@@ -25,31 +30,69 @@ const SelectSeat = () => {
     });
     return rs;
   };
-  const renderSeatsBtn = () => {
-    let rs = totalSeats.map((e,i) =>{
-      return <Button 
-        key={i}
-        TypeTagChild={'Text'}
-        content={`${i+1}`}
-        style={[styles.btnSeats,{marginBottom:'2%',marginHorizontal:'1%'}]}
-        ContentStyle={{color:'white'}}/>
+  const isFound = (seat) => {
+    let rs = SelectedSeats.some(Element => {
+      if(Element.id === seat.id){
+        return true
+      }
+      return false
     })
     return rs
   }
+  const removeElement = (e) =>{
+    let array = Array.from(SelectedSeats)
+    let index = array.findIndex((Element) => Element.id === e.id && Element.lable === e.lable)
+    if(index !== -1){
+      array.splice(index, 1);
+      setSelectedSeats(array)
+    }
+  }
+
+  const onPressSeat = (e) =>{
+      if(!isFound(e)){
+        setSelectedSeats(SelectedSeats.concat(e))
+      }else{
+        removeElement(e)
+      }
+  }
+
+  const renderSeatsBtn = () => {
+    let rs = totalSeats.map((e) => {
+      return (
+        <Button
+          key={e.id}
+          TypeTagChild={"Text"}
+          content={e.lable}
+          ContentStyle={{ color: "white" }}
+          onPress={() => onPressSeat(e)}
+          ContainerStyle={[
+            styles.btnSeats,
+            { marginBottom: "2%", marginHorizontal: "1%" },
+            isFound(e) ? {backgroundColor:'rgba(255, 128, 54, 1)'} : {}
+          ]}
+        />
+      );
+    });
+    return rs;
+  };
   return (
     <View style={styles.container}>
-      <SelectSeatsBar ZoomPress={() => setisZoom(!isZoom)}/>
+      <SelectSeatsBar ZoomPress={() => setisZoom(!isZoom)} />
       <View style={styles.TagList}>
         <StausIcon status={"Available"} text={"Available"} />
         <StausIcon status={"Occupied"} text={"Occupied"} />
         <StausIcon status={"Chosen"} text={"Chosen"} />
       </View>
-      <ScrollView scrollEnabled={true} contentContainerStyle={styles.ListIcon} style={{}}>
+      <ScrollView
+        scrollEnabled={true}
+        contentContainerStyle={styles.ListIcon}
+        style={{}}
+      >
         <Image
           source={require("../../assets/img/icons/Screen.png")}
-          style={{ width: "100%"}}
+          style={{ width: "100%" }}
         />
-        {isZoom !==true ? renderIcon() : renderSeatsBtn()}
+        {isZoom !== true ? renderIcon() : renderSeatsBtn()}
       </ScrollView>
       {/* <Button
         content={'Buy 2 tickets • 3200 ₸'}
