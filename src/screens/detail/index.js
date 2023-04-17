f6ebb7464b018a0e6dab02cafa6e3ab19752f54f
Hung Dung <truonghungdung007@gmail.com>
2023-04-17 12:27:46 +0700

    rt yfyh

--- src/screens/detail/index.js
+++ src/screens/detail/index.js
@@ -1,6 +1,5 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import styles from './styles';
 import SwitchBar from '../../components/TopBar/SwitchBar';
 
 const DetailScreen = () => {
