bdc485fb40ffb05e0b1fe6a547360bb88ba36bdc
Hung Dung <truonghungdung007@gmail.com>
2023-04-26 20:54:08 +0700

    Merge branch 'Backup_develop_Hungdung' of https://github.com/namnh98/movie_app_clone into Backup_develop_Hungdung
    # Conflicts:
    #	package.json
    #	src/redux/actions.js
    #	src/redux/reducers.js
    #	src/redux/store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 import { createStore } from 'redux'
 import rootReducers from './reducers'
 
 const store = createStore(rootReducers)
 
 export default store
-=======
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reducers from './reducers';
-
-const composedEnhancers = composeWithDevTools();
-
-const store = createStore(reducers, composedEnhancers);
-
-export default store;
->>>>>>> c8e67687e9d1ef82d11d01612d7d09260ebaef63
